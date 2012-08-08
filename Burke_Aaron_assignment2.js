f6e0dedeb81a55530b798d9acd9325a2b11813a7
adburke <adburke@fullsail.edu>
2012-08-07 21:09:17 -0500

    JS work

--- Burke_Aaron_assignment2.js
+++ Burke_Aaron_assignment2.js
@@ -2,6 +2,43 @@
 	Aaron Burke
 	08/06/2012
 	Project 2
+	Making Spaghetti
 */
+
+var ingredients = ["pasta", "sauce", "meatballs", "mushrooms", "onions"];
+var readyToCook = true;
+var boiling = true;
+var boilTimeMin = 8;
+var cupsOfWater = 8;
+var pastaBags = 1;
+
+// Procedure Function
+var readyCheck = function (state) {
+	if (state) {
+		console.log("We are ready to cook some spaghetti!");
+	}
+	else {
+		console.log("Get your act together we are hungry!");
+	}
+};
+
+var cookPasta = function (waterBoil, minutesBoiling) {
+	if ((waterBoil) && (minutesBoiling < 8 )) {
+		console.log("The pasta is still cooking.");
+		stirPasta(minutesBoiling);
+	}
+	else {
+		console.log("The pasta is finished!");
+	}
+	return true;
+};
+
+
+readyCheck(readyToCook);
+
+
+
+
+
 
 
